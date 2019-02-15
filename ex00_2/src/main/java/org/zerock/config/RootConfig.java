941e2384af3e6403e11540be8a2a76e834a6a26f
sonyo <sonyo@DESKTOP-FBS7QC9>
2019-02-15 10:11:28 +0900

    hikaricp mysql 연동

--- ex00_2/src/main/java/org/zerock/config/RootConfig.java
+++ ex00_2/src/main/java/org/zerock/config/RootConfig.java
@@ -3,6 +3,9 @@ package org.zerock.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +14,7 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
-@ComponentScan(basePackages= {"org.zerock.sample"})
+@ComponentScan(basePackages= {"org.zerock.mapper"})
 public class RootConfig {
 	
 	//RootConfig 에서 @ComponentScan 어노테이션을 이용해서 처리 할 수 있다 
@@ -19,16 +22,26 @@ public class RootConfig {
 	@Bean
 	public DataSource dataSource(){
 		HikariConfig hikariConfig= new HikariConfig();
-		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
-		hikariConfig.setUsername("book_ex");
-		hikariConfig.setPassword("book_ex");
+//		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+//		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
+//		hikariConfig.setUsername("book_ex");
+//		hikariConfig.setPassword("book_ex");
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:mysql://127.0.0.1:3306/book_ex?useSSL=false");
+		hikariConfig.setUsername("yuni");
+		hikariConfig.setPassword("1234");
 		
 		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
 		return  dataSource;
 		
 	}
-	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		SqlSessionFactoryBean sqlSessionFactory= new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		
+		return sqlSessionFactory.getObject();
+	}
 
 		
 }
