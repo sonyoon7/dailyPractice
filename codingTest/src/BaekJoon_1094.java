47c4a887b5bd1ecb952a948d5fc1903ded9c3c63
EuniceSon <sonyoon7@gmail.com>
2019-05-13 12:34:14 +0900

    백준알고리즘 막대기

--- codingTest/src/BaekJoon_1094.java
+++ codingTest/src/BaekJoon_1094.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class BaekJoon_1094 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int original_bar= 64;
+        int bar = sc.nextInt();
+        int sum_bar=0;
+        int div_bar=original_bar;
+        while (div_bar>bar){
+            System.out.println(div_bar);
+         div_bar= div_bar/2;
+        }
+
+    }
 }
