452dd8766f00df3059bedacfa151b59a0e85853b
EuniceSon <sonyoon7@gmail.com>
2019-05-14 11:06:53 +0900

    백준알고리즘 막대기 doene

--- codingTest/src/BaekJoon_1094.java
+++ codingTest/src/BaekJoon_1094.java
@@ -2,15 +2,21 @@ import java.util.Scanner;
 
 public class BaekJoon_1094 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int original_bar= 64;
-        int bar = sc.nextInt();
-        int sum_bar=0;
-        int div_bar=original_bar;
-        while (div_bar>bar){
-            System.out.println(div_bar);
-         div_bar= div_bar/2;
-        }
+        Scanner sc = new Scanner(System.in);
+        int length = 64;
+        int x = sc.nextInt();
+        int count = 0;
+        int sum = 0;
 
+        while (x > 0) {
+
+            if (length > x) {
+                length /= 2;
+            } else {
+                count++;
+                x -= length;
+            }
+        }
+        System.out.println(count);
     }
 }
